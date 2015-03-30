0510d170e2408136a525baf912e5e353e48f4dbe
Jeffrey Wan <Jwan622@gmail.com>
2015-03-30 17:53:10 -0600

    commiting changes so far

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,14 +2,6 @@ class Link < ActiveRecord::Base
   before_create :shorten_url
   before_create :set_defaults
 
-  def self.sort_by_most_recent
-    order(created_at: :desc, clicks: :desc)
-  end
-
-  def self.sort_by_most_views
-    order(clicks: :desc, created_at: :desc).all
-  end
-
   def shorten_url
     self.short_url = "bit.ly-remix/" + SecureRandom.urlsafe_base64(6)
   end
@@ -17,7 +9,7 @@ class Link < ActiveRecord::Base
   def set_defaults
     self.clicks = 0 if clicks.blank?
     if title.blank? && orig_url =~ /.com/ # this regex needs work.
-      self.title = orig_url.split(".")[1] + " Link"
+      self.title = TitleParser.new(orig_url).obtain_title
     else
       self.title = orig_url
     end
