a9d8209afdc7347ebfcf75fd032b317140bed335
Jeffrey Wan <Jwan622@gmail.com>
2015-03-30 08:48:10 -0600

    finished filter

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
 
   function renderLinks(json_data) {
     var checkedValue = checkedBoxes();
-    debugger
     if (checkedValue === "created_at") {
       json_data.sort(function(a,b) {
         return Date.parse(b[checkedValue]) - Date.parse(a[checkedValue])
@@ -20,7 +19,7 @@ $(document).ready(function() {
     }
     else if (checkedValue === "clicks") {
       json_data.sort(function(a,b) {
-        return b[checkedValue] - a[checkedValue]
+        return parseInt(b[checkedValue]) - parseInt(a[checkedValue])
       });
     };
     $(".all-links").html(JST["templates/links"]({ sorted: json_data }))
@@ -29,7 +28,7 @@ $(document).ready(function() {
   if ($(".all-links").length > 0) {
     $.getJSON("links.json", function(links_json) {
       renderLinks(links_json);
-      checkboxForm.find(":checkbox").on("click", renderLinks.bind(null, links_json))
+      checkboxForm.find(":radio").on("click", renderLinks.bind(null, links_json))
     });
   };
 });
