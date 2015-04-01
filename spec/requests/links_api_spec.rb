1f5c30fe542e39ee6b338911dc49ca4c8a38453b
Jeffrey Wan <Jwan622@gmail.com>
2015-03-31 23:35:39 -0600

    fixed test

--- spec/requests/links_api_spec.rb
+++ spec/requests/links_api_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe "Links endpoint", type: :request do
   describe "GET links endpoint" do
     it "should return back json data of all links" do
-      link1 = create(:link)
-      link2 = create(:link)
+      create(:link)
+      create(:link)
       get api_v1_links_path(format: :json)
 
       links = JSON.parse(response.body)
